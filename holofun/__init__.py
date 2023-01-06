a59678d539b9c568f26ad1e3d54eed6176ad6e2b
Will Hendricks <hendricksw@gmail.com>
2023-01-06 14:39:42 -0800

    scope utils

--- holofun/__init__.py
+++ holofun/__init__.py
@@ -2,3 +2,10 @@ import logging
 # custom logging
 logformat = '{relativeCreated:08.0f} - {levelname:8} - [{module}:{funcName}:{lineno}] - {message}'
 logging.basicConfig(format=logformat, style='{')
+
+# scope some useful things locally
+from .utils import (
+    make_paths,
+    nbsetup,
+    make_results_folder
+)

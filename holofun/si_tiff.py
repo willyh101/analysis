f41806f8bfda561dae19af08ba0e35ae84bf3ee7
Will Hendricks <hendricksw@gmail.com>
2023-07-25 15:14:22 -0700

    add new interface for reading tiffs

--- holofun/si_tiff.py
+++ holofun/si_tiff.py
@@ -1,7 +1,11 @@
-import numpy as np
-from pathlib import Path
 import pickle
+import warnings
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+import numpy as np
 from ScanImageTiffReader import ScanImageTiffReader
+from tifffile import TiffFile
 
 try:
     from tqdm import tqdm
@@ -9,54 +13,187 @@ try:
 except ModuleNotFoundError:
     HAS_TQDM = False
 
-class SItiffCore:
-    def __init__(self, path) -> None:
-        """
-        Loads a ScanImage tiff from the specified path.
-
-        Args:
-            path (str or Path): filepath to tiff
-        """
+class AbstractReader(ABC):
+    def __init__(self, path: Path) -> None:
         self.path = str(path)
         
-        with ScanImageTiffReader(self.path) as reader:
-            self.data = reader.data()
-            self._metadata = reader.metadata()
-        
-        self.metadata = metadata_to_dict(self._metadata)
-            
-    def _eval_numeric_metadata(self, key):
-        return eval(self.metadata[key].replace(' ',',').replace(';',','))
+    @abstractmethod
+    def read_metadata(self) -> dict:
+        """Reads the metadata from the file and returns it as a dict."""
+        pass
     
-    @property
-    def nchannels(self):
-        chans = self._eval_numeric_metadata('channelSave')
+    @abstractmethod
+    def read_data(self) -> np.ndarray:
+        """Reads the data from the file and returns it as a numpy array."""
+        pass
+    
+    def _get_nchannels(self, chans):
         if isinstance(chans, int):
             nchannels = 1
         else:
             nchannels = len(chans)
         return nchannels
     
-    @property
-    def zs(self):
-        zs = self._eval_numeric_metadata('zs')
+    def _get_zs(self, zs):
         if isinstance(zs, (int, float)):
             zs = [zs]
         return zs
     
-    @property
-    def nplanes(self):
-        return len(self.zs)
+    def _get_nplanes(self, zs):
+        if isinstance(zs, (int, float)):
+            zs = [zs]
+        return len(zs)
     
-    @property
-    def fr(self):
-        return self._eval_numeric_metadata('scanVolumeRate')
+    def _get_fr(self, scanVolumeRate):
+        return scanVolumeRate
+    
+class SItiffDataReader(AbstractReader):
+    def __init__(self, path: Path) -> None:
+        super().__init__(path)
+        self.data = self.read_data()
+        self._metadata = self.read_metadata()
+        self.metadata = self.parse_metadata(self._metadata)
+        self.reader_backend = 'scanimage'
+        
+    def read_data(self) -> np.ndarray:
+        with ScanImageTiffReader(self.path) as reader:
+            data = reader.data()
+        return data
+            
+    def read_metadata(self):
+        with ScanImageTiffReader(self.path) as reader:
+            metadata = reader.metadata()
+        return metadata
+    
+    def read_both(self):
+        with ScanImageTiffReader(self.path) as reader:
+            data = reader.data()
+            _metadata = reader.metadata()
+        return data, _metadata
+    
+    def parse_metadata(self, metadata) -> dict:
+        """Read the SI metadata and turn in into a dict. Does not cast/eval values."""
+        # split at the new line marker
+        meta = metadata.split('\n')
+        # filter out the ROI data by keeping fields that start with SI.
+        meta = list(filter(lambda x: 'SI.' in x, meta))
+        # make dictionary by splitting at equals, and only keep the last part of the fieldname as the key
+        d = {k.split('.')[-1]:v for k,v in (entry.split(' = ') for entry in meta)}
+        return d
+    
+    def _eval_numeric_metadata(self, key):
+        return eval(self.metadata[key].replace(' ',',').replace(';',','))
     
     def description(self, frame):
         with ScanImageTiffReader(self.path) as reader:
             header = reader.description(frame)
         return header
     
+    @property
+    def nchannels(self):
+        chan = self._eval_numeric_metadata('channelSave')
+        return self._get_nchannels(chan)
+    
+    @property
+    def zs(self):
+        zs = self._eval_numeric_metadata('zs')
+        return self._get_zs(zs)
+    
+    @property
+    def nplanes(self):
+        zs = self._eval_numeric_metadata('zs')
+        return self._get_nplanes(zs)
+    
+    @property
+    def fr(self):
+        fr = self._eval_numeric_metadata('scanVolumeRate')
+        return self._get_fr(fr)
+
+
+class TiffFileDataReader(AbstractReader):
+    def __init__(self, path: Path) -> None:
+        super().__init__(path)
+        self.data = self.read_data()
+        self._metadata = self.read_metadata()
+        self.metadata = self.parse_metadata(self._metadata)
+        self.reader_backend = 'tifffile'
+    
+    def read_data(self) -> np.ndarray:
+        with TiffFile(self.path) as reader:
+            data = reader.asarray().copy()
+        return data
+    
+    def read_metadata(self) -> dict:
+        with TiffFile(self.path) as reader:
+            metadata = reader.scanimage_metadata
+        return metadata
+    
+    def parse_metadata(self, metadata) -> dict:
+        meta = {k:v for k,v in metadata['FrameData'].items() if k.startswith('SI.')}
+        d = {k.split('.')[-1]:v for k,v in meta.items()}
+        return d
+    
+    @property
+    def nchannels(self):
+        chan = self.metadata['channelSave']
+        return self._get_nchannels(chan)
+    
+    @property
+    def zs(self):
+        zs = self.metadata['zs']
+        return self._get_zs(zs)
+    
+    @property
+    def nplanes(self):
+        zs = self.metadata['zs']
+        return self._get_nplanes(zs)
+    
+    @property
+    def fr(self):
+        fr = self.metadata['scanVolumeRate']
+        return self._get_fr(fr)
+    
+
+def get_tslice(z_idx: int, ch_idx: int, nchannels: int, nplanes: int):
+    """
+    Get the slice of the data corresponding to a specified zplane and channel.
+
+    Args:
+        z_idx (int): z-plane index
+        ch_idx (int): channel index
+        nchannels (int): total number of channels
+        nplanes (int): total number of z-planes
+
+    Returns:
+        slice: slicer into the data array
+    """
+    return slice((z_idx*nchannels)+ch_idx, None, nplanes*nchannels)
+
+class SItiffCore:
+    def __init__(self, path: Path, backend: AbstractReader = None) -> None:
+        self.path = str(path)
+        
+        if backend is None:
+            self.backend = SItiffDataReader
+        else:
+            self.backend = backend
+        
+        try:
+            self._reader = self.backend(self.path)
+        except Exception:
+            warnings.warn(f'Could not read {self.path} with {self.backend}. Trying TiffFileDataReader.')
+            self.backend = TiffFileDataReader
+            self._reader = self.backend(self.path)
+        
+        self.data = self._reader.data
+        self._metadata = self._reader._metadata
+        self.metadata = self._reader.metadata
+        
+        self.nchannels = self._reader.nchannels
+        self.zs = self._reader.zs
+        self.nplanes = self._reader.nplanes
+        self.fr = self._reader.fr
+        
     def extract(self, z, ch):
         """
         Get the underlying data for a specified zplane and channel. 
@@ -72,24 +209,6 @@ class SItiffCore:
         img = self.data[tslice,:,:]
         return img
     
-def load_si_metadata_as_dict(file) -> dict:
-    """Read the SI metadata and turn in into a dict. Does not cast/eval values."""
-    with ScanImageTiffReader(file) as reader:
-        meta = reader.metadata()
-    d = metadata_to_dict(meta)
-    return d
-
-def metadata_to_dict(meta) -> dict:
-    """Read the SI metadata and turn in into a dict. Does not cast/eval values."""
-    # split at the new line marker
-    meta = meta.split('\n')
-    # filter out the ROI data by keeping fields that start with SI.
-    meta = list(filter(lambda x: 'SI.' in x, meta))
-    # make dictionary by splitting at equals, and only keep the last part of the fieldname as the key
-    d = {k.split('.')[-1]:v for k,v in (entry.split(' = ') for entry in meta)}
-    return d
-    
-
 
 def slice_movie(mov_path, x_slice, y_slice, t_slice) -> np.ndarray:
     """
@@ -105,10 +224,13 @@ def slice_movie(mov_path, x_slice, y_slice, t_slice) -> np.ndarray:
     Returns:
         np.array: array of sliced movie
     """
-    with ScanImageTiffReader(mov_path) as reader:
-        data = reader.data()
-        data = data[t_slice, y_slice, x_slice]
+    data = SItiffCore(mov_path).data
+    data = data[t_slice, y_slice, x_slice]
     return data
+    
+def tiffs2array(movie_list, x_slice, y_slice, t_slice):
+    data = [slice_movie(str(mov), x_slice, y_slice, t_slice) for mov in movie_list]
+    return np.concatenate(data)
 
 def count_tiff_lengths(movie_list, save=False):
     """
@@ -144,10 +266,6 @@ def count_tiff_lengths(movie_list, save=False):
     
     return np.array(nframes)
 
-def tiffs2array(movie_list, x_slice, y_slice, t_slice):
-    data = [slice_movie(str(mov), x_slice, y_slice, t_slice) for mov in movie_list]
-    return np.concatenate(data)
-
 def get_crop_mask(Cx, Cy, bb):
     """
     Returns a mask to crop an image around a centered x,y point. Uses advanced indexing.
@@ -161,6 +279,3 @@ def get_crop_mask(Cx, Cy, bb):
     ys = np.arange(Cy-bb, Cy+bb, dtype=int)
     mask = np.ix_(ys,xs)
     return mask
-
-def get_tslice(z_idx, ch_idx, nchannels, nplanes):
-    return slice((z_idx*nchannels)+ch_idx, None, nplanes*nchannels)
--- tests/test_tiffs.py
+++ tests/test_tiffs.py
@@ -1,7 +1,54 @@
+from turtle import back
+import numpy as np
+import py
 import pytest
+from holofun.tiff_readers import SItiffCore, TiffFileDataReader, SItiffDataReader
 
 @pytest.fixture
-def readers():
-    from holofun.tiff_readers import READERS
-    
+def good_file():
+    return '/mnt/data2/experiments/w57_1/20230125/20230125_w57_1_img1020_00001.tif'
 
+@pytest.fixture
+def test_file_fails():
+    return '/mnt/scratch/masato/FOV1.tif'
+
+@pytest.fixture(params=[SItiffDataReader, TiffFileDataReader])
+def reader_backend(request):
+    return request.param
+
+@pytest.fixture
+def tif(good_file, reader_backend):
+    return SItiffCore(good_file, backend=reader_backend)
+
+NPLANES = 3
+NCHANNELS = 2
+
+# pytestmark = pytest.mark.parametrize('backend', [SItiffDataReader, TiffFileDataReader])
+
+def test_data(tif):
+    assert isinstance(tif.data, np.ndarray)
+    
+def test_metadata(tif):
+    assert isinstance(tif.metadata, dict)
+    
+def test_extract(tif):
+    tmp = tif.extract(0, 0)
+    assert isinstance(tmp, np.ndarray)
+    
+def test_nplanes(tif):
+    assert tif.nplanes == NPLANES
+    assert isinstance(tif.nplanes, int)
+    
+def test_nchannels(tif):
+    assert tif.nchannels == NCHANNELS
+    assert isinstance(tif.nchannels, int)
+    
+def test_zs(tif):
+    assert isinstance(tif.zs, list)
+    assert len(tif.zs) == NPLANES
+    assert isinstance(tif.zs[0], (float, int))
+    
+def test_backend_failover_to_tifffile(test_file_fails):
+    tif = SItiffCore(test_file_fails, backend=SItiffDataReader)
+    assert isinstance(tif._reader, TiffFileDataReader)
+    assert tif._reader.reader_backend == 'tifffile'
